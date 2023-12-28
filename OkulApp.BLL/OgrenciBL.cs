330e72468cee25b096eae6d09c1de16f318ac378
Dell <Dell@RAYDLOF>
2023-12-28 14:54:49 +0300

    Add project files.

---
+++ OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,27 @@
+﻿using OkulApp.MODEL;
+using System;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using DAL;
+using System.Text;
+using System.Collections.Generic;
+
+namespace OkulApp.BLL
+{
+    public class OgrenciBL
+    {
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            SqlParameter[] p = {
+                             new SqlParameter("@Ad",ogr.Ad),
+                             new SqlParameter("@Soyad",ogr.Soyad),
+                             new SqlParameter("@Numara",ogr.Numara)
+                         };
+
+            Helper hlp = new Helper();
+            return hlp.ExecuteNonQuery("Insert into tblOgrenciler values(@Ad,@Soyad,@Numara)", p)>0;
+
+        }
+    }
+}
+//n Katmanlı Mimari
---
+++ OkulApp.BLL/OgretmenBL.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using OkulApp.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace OkulApp.BLL
+{
+    public class OgretmenBL
+    {
+        public bool OgretmenEkle(Ogretmen ogrt)
+        {
+            SqlParameter[] p = {
+                             new SqlParameter("@Ad",ogrt.Ad),
+                             new SqlParameter("@Soyad",ogrt.Soyad),
+                             new SqlParameter("@TCkimlik",ogrt.TCkimlik)
+                         };
+            Helper hlp = new Helper();
+            return hlp.ExecuteNonQuery("Insert into tblOgretmenlerr values(@Ad, @Soyad, @TCkimlik)", p) > 0;
+        }
+    }
+}
